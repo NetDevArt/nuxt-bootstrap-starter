a63ce69eae7e189481e2d0dae85c0a935c226593
Arthur DEVAUX <adevaux@seela.io>
2022-10-16 12:39:04 +0200

    Update dependencies. Update nuxt from rc-9 to rc-11. Clean npm cache, node_modules, and package lock.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt/config"
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     meta: {
-        title: 'Site title',
+        title: "Site title",
     },
     typescript: {
         strict: true,
     },
-    css: ['~/assets/scss/theme.scss'],
+    css: ["~/assets/scss/theme.scss"],
 })
